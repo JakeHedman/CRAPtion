03fef9a829b50fb978ada29ba1f34b70fbbe23ef
spillevink <jakob.hedman+github@gmail.com>
2013-02-21 23:50:05 +0100

    Update dropbox_upload.py

--- dropbox_upload.py
+++ dropbox_upload.py
@@ -15,9 +15,3 @@ def upload(local_path, filename, dropconf):
 	client = dropbox.client.DropboxClient(sess)
 	uploaded = client.put_file("/" + filename, open(local_path))
 	return client.share(uploaded['path'])['url']
-
-upload(
-	"/Users/jakob/craptions/craption-kYANB-1360027818-2013-30-05 02:02:18.png",
-	"craption-kYANB-1360027818-2013-30-05 02:02:18.png",
-	settings.get_conf()['upload']['dropbox']
-)
